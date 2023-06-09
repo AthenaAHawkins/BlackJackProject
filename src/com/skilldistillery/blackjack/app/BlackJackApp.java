716bbc1bb45505a575e8d01200454d874cc9dd0b
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-06-09 14:27:59 -0600

    started classes for game

---
+++ src/com/skilldistillery/blackjack/app/BlackJackApp.java
@@ -0,0 +1,22 @@
+package com.skilldistillery.blackjack.app;
+
+public class BlackJackApp {
+public static void main(String[] args) {
+	BlackJackApp blackJack = new BlackJackApp();
+	blackJack.run();
+}
+public void run() {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+}
